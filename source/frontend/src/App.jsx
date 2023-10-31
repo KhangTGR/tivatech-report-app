365bd9dd3f443934bd7fc436a90e4a7fd2f57963
Khangdepzai <khangtgr@gmail.com>
2023-10-31 10:51:03 +0700

    Complete LoginForm

---
+++ source/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './App.css'
+
+import Dashboard from './pages/dashboard/Dashboard'
+// import NoPage from "./pages/error/NoPage"
+// import SignIn from "./pages/sign-in/SignIn"
+
+function App() {
+  return (
+    <Dashboard></Dashboard>
+  )
+}
+
+export default App
